4a01eae1b0537cf851b8f30ea8243e04638abd99
Anthony Le Goas <anthony.legoas@gmail.com>
2020-04-13 10:50:53 +0200

    🚚 (cdk) move <Nav> to cdk

--- src/cdk/navigation/Nav/Nav.svelte
+++ src/cdk/navigation/Nav/Nav.svelte
@@ -7,6 +7,7 @@
     border-bottom: 1px solid rgba(255, 62, 0, 0.1);
     font-weight: 300;
     padding: 0 1em;
+    height: 100%;
   }
 
   ul {
@@ -60,9 +61,6 @@
         about
       </a>
     </li>
-
-    <!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
-		     the blog data when we hover over the link or tap it on a touchscreen -->
     <li>
       <a
         rel="prefetch"
