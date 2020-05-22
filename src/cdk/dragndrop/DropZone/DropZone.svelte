25637ddf2aa0f800bf18fd8f2d4ed6ca2f1e8c1a
Anthony Le Goas <anthony.legoas@gmail.com>
2020-05-22 12:57:37 +0200

    ✨ (cdk) add <DropZone>

---
+++ src/cdk/dragndrop/DropZone/DropZone.svelte
@@ -0,0 +1,17 @@
+<style>
+  .drop_zone {
+    width: 270px;
+    height: 270px;
+    margin: 10px;
+  }
+</style>
+
+<div
+  class="drop_zone"
+  id={$$props.id}
+  on:dragenter
+  on:dragleave
+  on:drop
+  ondragover="return false">
+  <slot />
+</div>
