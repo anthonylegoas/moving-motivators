97ce334cfc032b9fe3b2deee1546fcfc80818692
Anthony Le Goas <anthony.legoas@gmail.com>
2020-05-22 13:11:10 +0200

    ✨ (cdk) add <Draggable>

---
+++ src/cdk/dragndrop/Draggable/Draggable.svelte
@@ -0,0 +1,9 @@
+<div
+  draggable="true"
+  on:dragstart
+  on:dragend
+  on:touchstart
+  on:touchmove
+  on:touchend>
+  <slot />
+</div>
--- src/cdk/dragndrop/index.js
+++ src/cdk/dragndrop/index.js
@@ -1,1 +1,2 @@
+export { default as Draggable } from "./Draggable";
 export { default as DropZone } from "./DropZone";
