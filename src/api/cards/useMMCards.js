13ce847fe91aeaddad9e9f080c65521ec1f8cedb
Anthony Le Goas <anthony.legoas@gmail.com>
2020-05-22 15:57:01 +0200

    ✨ (api) add useMMCards hook

---
+++ src/api/cards/useMMCards.js
@@ -0,0 +1,61 @@
+/**
+ * Allow to retrieve Moving Motivators cards.
+ */
+const useMMCards = () => {
+  const cards = [
+    {
+      id: 1,
+      img: "images/motivators/fr/acceptance.png",
+      name: "Acceptation",
+    },
+    {
+      id: 2,
+      img: "images/motivators/fr/curiosity.png",
+      name: "Curiosité",
+    },
+    {
+      id: 3,
+      img: "images/motivators/fr/freedom.png",
+      name: "Liberté",
+    },
+    {
+      id: 4,
+      img: "images/motivators/fr/goal.png",
+      name: "But",
+    },
+    {
+      id: 5,
+      img: "images/motivators/fr/honor.png",
+      name: "Honneur",
+    },
+    {
+      id: 6,
+      img: "images/motivators/fr/mastery.png",
+      name: "Maîtrise",
+    },
+    {
+      id: 7,
+      img: "images/motivators/fr/order.png",
+      name: "Ordre",
+    },
+    {
+      id: 8,
+      img: "images/motivators/fr/power.png",
+      name: "Pouvoir",
+    },
+    {
+      id: 9,
+      img: "images/motivators/fr/relatedness.png",
+      name: "Relations socials",
+    },
+    {
+      id: 10,
+      img: "images/motivators/fr/status.png",
+      name: "Statut",
+    },
+  ];
+
+  return cards;
+};
+
+export default useMMCards;
