b0a2b8be56a8d047719263b94de4976f531da7c1
Anthony Le Goas <anthony.legoas@gmail.com>
2020-05-23 11:43:14 +0200

    ✨ (cdk) add <Icon>

---
+++ src/cdk/icons/Icon/Icon.svelte
@@ -0,0 +1,30 @@
+<script>
+  import Icons from "./_Icons.svelte";
+
+  export let name;
+  export let size = 20;
+</script>
+
+<style>
+  .icon {
+    position: relative;
+    overflow: hidden;
+    vertical-align: middle;
+    -o-object-fit: contain;
+    object-fit: contain;
+    -webkit-transform-origin: center center;
+    transform-origin: center center;
+    stroke: currentColor;
+    stroke-width: 2;
+    stroke-linecap: round;
+    stroke-linejoin: round;
+    fill: none;
+    margin: 0.25em;
+    padding: 0.25em;
+  }
+</style>
+
+<svg class="icon" width={size} height={size}>
+  <Icons />
+  <use xlink:href="#{name}" />
+</svg>
