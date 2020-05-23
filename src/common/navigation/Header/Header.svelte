4d44a3eec3088242ada70229eb1bef1cfa5e1b53
Anthony Le Goas <anthony.legoas@gmail.com>
2020-05-23 16:43:14 +0200

    💄 (common) animate <Header> content

--- src/common/navigation/Header/Header.svelte
+++ src/common/navigation/Header/Header.svelte
@@ -1,6 +1,7 @@
 <script>
   import Nav from "./Nav";
   import { menuStore } from "../../../stores";
+  import { fly } from "svelte/transition";
 
   export let segment;
 
@@ -15,6 +16,9 @@
     flex-direction: column;
     justify-content: center;
     align-items: flex-start;
+  }
+
+  .header-content {
     padding: 0.5em 2em;
   }
 
@@ -36,19 +40,26 @@
   }
 </style>
 
-<header class:open={$menuIsOpen}>
-  <a href=".">
-    <img
-      alt="Management 3.0 logo"
-      class="logo"
-      src="images/logo/m30-logo.png" />
-  </a>
-  <h1>Moving Motivators</h1>
-  <p>
-    Issu du Management 3.0, cet exercice permet aux personnes de réfléchir sur
-    leurs motivations et comment les changements dans l'organisation de
-    l'entreprise peuvent les affecter.
-  </p>
+<header>
+  {#if $menuIsOpen}
+    <div
+      class="header-content"
+      in:fly={{ delay: 400, duration: 250, x: -100, y: 0 }}
+      out:fly={{ delay: 0, duration: 100, x: -100, y: 0 }}>
+      <a href=".">
+        <img
+          alt="Management 3.0 logo"
+          class="logo"
+          src="images/logo/m30-logo.png" />
+      </a>
+      <h1>Moving Motivators</h1>
+      <p>
+        Issu du Management 3.0, cet exercice permet aux personnes de réfléchir
+        sur leurs motivations et comment les changements dans l'organisation de
+        l'entreprise peuvent les affecter.
+      </p>
 
-  <Nav {segment} />
+      <Nav {segment} />
+    </div>
+  {/if}
 </header>
