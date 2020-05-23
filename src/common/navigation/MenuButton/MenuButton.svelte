0c6cac08be9d7803e593847cbb6d759c110f107a
Anthony Le Goas <anthony.legoas@gmail.com>
2020-05-23 11:45:59 +0200

    ✨ (common) add <MenuButton>

---
+++ src/common/navigation/MenuButton/MenuButton.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { menuStore } from "../../../stores";
+  import { IconButton } from "../../../cdk";
+
+  const { menuIsOpen, toggleMenu } = menuStore;
+</script>
+
+<IconButton
+  class={$$props.class}
+  name={$menuIsOpen ? 'arrow-left' : 'arrow-right'}
+  on:click={() => toggleMenu()} />
