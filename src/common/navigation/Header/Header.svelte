b80e2862ddce848c127a35fcb1e4f957504e084f
Anthony Le Goas <anthony.legoas@gmail.com>
2020-05-24 21:53:19 +0200

    🎨 (common) improve <Header> structure

--- src/common/navigation/Header/Header.svelte
+++ src/common/navigation/Header/Header.svelte
@@ -28,10 +28,13 @@
     margin: 1em 0;
   }
 
-  h1 {
+  p.app-name {
+    font-family: "KapraNeue-Regular";
     font-size: 6rem;
     color: var(--primary-color);
-    margin: 0;
+    margin: 0 0 0.5em 0;
+    font-weight: 400;
+    line-height: 1.2;
   }
 
   p {
@@ -52,11 +55,15 @@
           class="logo"
           src="images/logo/m30-logo.png" />
       </a>
-      <h1>Moving Motivators</h1>
+      <p class="app-name">Moving Motivators</p>
       <p>
-        Issu du Management 3.0, cet exercice permet aux personnes de réfléchir
-        sur leurs motivations et comment les changements dans l'organisation de
-        l'entreprise peuvent les affecter.
+        Issu du
+        <a href="https://management30.com/learn/" target="_blank">
+          Management 3.0
+        </a>
+        , cet exercice permet aux personnes de réfléchir sur leurs motivations
+        et comment les changements dans l'organisation de l'entreprise peuvent
+        les affecter.
       </p>
 
       <Nav {segment} />
