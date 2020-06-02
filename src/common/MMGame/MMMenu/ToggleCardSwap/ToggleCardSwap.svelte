dc26ff1a42429f7df5b841a87c5b155ca73c2cbe
Anthony Le Goas <anthony.legoas@gmail.com>
2020-06-02 21:51:36 +0200

    ✨ (common) add <ToggleCardSwap>

---
+++ src/common/MMGame/MMMenu/ToggleCardSwap/ToggleCardSwap.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { IconButton } from "../../../../cdk";
+  import MMMenuEntry from "../MMMenuEntry";
+
+  export let swapIsActive;
+</script>
+
+<MMMenuEntry active={swapIsActive}>
+  <IconButton name="swap" on:click />
+</MMMenuEntry>
