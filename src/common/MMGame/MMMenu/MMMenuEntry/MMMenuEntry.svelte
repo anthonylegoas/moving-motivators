21c9898adc9008742d6c7f42b663aed0583e144f
Anthony Le Goas <anthony.legoas@gmail.com>
2020-06-02 21:52:46 +0200

    ♻ (common) allow <MMMenuEntry> to be active or not

--- src/common/MMGame/MMMenu/MMMenuEntry/MMMenuEntry.svelte
+++ src/common/MMGame/MMMenu/MMMenuEntry/MMMenuEntry.svelte
@@ -1,16 +1,24 @@
+<script>
+  export let active = false;
+</script>
+
 <style>
   .mm-menu-entry {
-    margin: 0.5em 0;
+    margin: 0;
     user-select: none;
     transition: all ease 0.2s;
   }
-
+  .mm-menu-entry.active,
   .mm-menu-entry:hover {
     color: var(--primary-color);
     background: var(--primary-color-light);
   }
+
+  .mm-menu-entry.active {
+    box-shadow: inset 0 0 6px 2px rgba(0, 0, 0, 0.2);
+  }
 </style>
 
-<div class="mm-menu-entry">
+<div class="mm-menu-entry" class:active>
   <slot />
 </div>
