e92e5b5e632762840ab61aaedb3ee4c802caa0e5
Anthony Le Goas <anthony.legoas@gmail.com>
2020-06-02 21:51:36 +0200

    ✨ (common) add <ToggleCardSwap>

---
+++ src/common/MMGame/MMMenu/ToggleCardSwap/ToggleCardSwapContainer.svelte
@@ -0,0 +1,8 @@
+<script>
+  import ToggleCardSwap from "./ToggleCardSwap.svelte";
+  import { gameStore } from "../../../../stores";
+
+  const { swapIsActive, toggleSwap } = gameStore;
+</script>
+
+<ToggleCardSwap swapIsActive={$swapIsActive} on:click={() => toggleSwap()} />
