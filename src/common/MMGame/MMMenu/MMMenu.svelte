4c0a6ec31559addc133510b05c16c284b41e4cd6
Anthony Le Goas <anthony.legoas@gmail.com>
2020-06-07 21:44:12 +0200

    ♻ (common) move <MMMenu> relative to app menu

--- src/common/MMGame/MMMenu/MMMenu.svelte
+++ src/common/MMGame/MMMenu/MMMenu.svelte
@@ -1,22 +1,30 @@
 <script>
   import { MenuButton } from "../../navigation";
+  import { menuStore } from "../../../stores";
   import MMMenuEntry from "./MMMenuEntry";
   import ToggleCardSwap from "./ToggleCardSwap";
+
+  const { menuIsOpen } = menuStore;
 </script>
 
 <style>
   .mm-menu {
-    position: absolute;
+    position: fixed;
     top: 8em;
     left: 1em;
     padding: 1em 0;
     border-radius: 4px;
     box-shadow: var(--game-shadow);
     background-color: #fff;
+    transition: all 0.3s ease-in-out;
+  }
+
+  .mm-menu.with-app-menu {
+    left: calc(35% + 1em);
   }
 </style>
 
-<div class="mm-menu">
+<div class="mm-menu" class:with-app-menu={$menuIsOpen}>
   <MMMenuEntry>
     <MenuButton />
   </MMMenuEntry>
