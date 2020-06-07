3faa0832d07fe0a4339f582b81014ede269938da
Anthony Le Goas <anthony.legoas@gmail.com>
2020-06-07 12:59:59 +0200

    ♻ (api) use webp images in useMMCards

--- src/api/cards/useMMCards.js
+++ src/api/cards/useMMCards.js
@@ -5,52 +5,52 @@ const useMMCards = () => {
   const cards = [
     {
       id: 1,
-      img: "images/motivators/fr/acceptance.png",
+      img: "images/motivators/fr/acceptance.webp",
       name: "Acceptation",
     },
     {
       id: 2,
-      img: "images/motivators/fr/curiosity.png",
+      img: "images/motivators/fr/curiosity.webp",
       name: "Curiosité",
     },
     {
       id: 3,
-      img: "images/motivators/fr/freedom.png",
+      img: "images/motivators/fr/freedom.webp",
       name: "Liberté",
     },
     {
       id: 4,
-      img: "images/motivators/fr/goal.png",
+      img: "images/motivators/fr/goal.webp",
       name: "But",
     },
     {
       id: 5,
-      img: "images/motivators/fr/honor.png",
+      img: "images/motivators/fr/honor.webp",
       name: "Honneur",
     },
     {
       id: 6,
-      img: "images/motivators/fr/mastery.png",
+      img: "images/motivators/fr/mastery.webp",
       name: "Maîtrise",
     },
     {
       id: 7,
-      img: "images/motivators/fr/order.png",
+      img: "images/motivators/fr/order.webp",
       name: "Ordre",
     },
     {
       id: 8,
-      img: "images/motivators/fr/power.png",
+      img: "images/motivators/fr/power.webp",
       name: "Pouvoir",
     },
     {
       id: 9,
-      img: "images/motivators/fr/relatedness.png",
+      img: "images/motivators/fr/relatedness.webp",
       name: "Relations socials",
     },
     {
       id: 10,
-      img: "images/motivators/fr/status.png",
+      img: "images/motivators/fr/status.webp",
       name: "Statut",
     },
   ];
