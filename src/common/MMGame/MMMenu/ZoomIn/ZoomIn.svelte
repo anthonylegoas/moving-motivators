7808f8cc3034685a7345a7f3bbcf7dee1252f69d
Anthony Le Goas <anthony.legoas@gmail.com>
2020-10-04 19:41:20 +0200

    ✨ (common) add <ZoomIn>

---
+++ src/common/MMGame/MMMenu/ZoomIn/ZoomIn.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { IconButton } from "../../../../cdk";
+  import MMMenuEntry from "../MMMenuEntry";
+
+  export let disabled = false;
+</script>
+
+<MMMenuEntry {disabled}>
+  <IconButton ariaLabel="Zoomer" name="plus" on:click />
+</MMMenuEntry>
---
+++ src/common/MMGame/MMMenu/ZoomIn/ZoomInContainer.svelte
@@ -0,0 +1,11 @@
+<script>
+  import ZoomIn from "./ZoomIn.svelte";
+  import { gameStore } from "../../../../stores";
+
+  const { zoom, zoomIn, zoomMax } = gameStore;
+  let disabled = true;
+
+  zoom.subscribe(value => (disabled = value >= zoomMax));
+</script>
+
+<ZoomIn {disabled} on:click={() => zoomIn()} />
