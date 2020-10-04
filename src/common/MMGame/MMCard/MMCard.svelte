cbf16926ca87c5d95448f2a56aec195186270e2b
Anthony Le Goas <anthony.legoas@gmail.com>
2020-10-04 19:44:52 +0200

    ✨ (common) link <MMCard> size to zoom level

--- src/common/MMGame/MMCard/MMCard.svelte
+++ src/common/MMGame/MMCard/MMCard.svelte
@@ -1,15 +1,19 @@
 <script>
   import { Draggable } from "../../../cdk";
+  import { gameStore } from "../../../stores";
 
   export let id;
   export let img;
   export let name;
+
+  const { zoom } = gameStore;
+  const maxCardSize = 450;
+  let cardSize = maxCardSize / 2;
+  zoom.subscribe(value => (cardSize = maxCardSize * value));
 </script>
 
 <style>
   img.moving-motivator-card {
-    width: 270px;
-    height: 270px;
     box-shadow: var(--game-shadow);
     border-radius: 4px;
     cursor: move;
@@ -17,5 +21,10 @@
 </style>
 
 <Draggable on:dragstart>
-  <img alt={name} class="moving-motivator-card" {id} src={img} />
+  <img
+    alt={name}
+    class="moving-motivator-card"
+    {id}
+    src={img}
+    style={`width: ${cardSize}px; height: ${cardSize}px;`} />
 </Draggable>
