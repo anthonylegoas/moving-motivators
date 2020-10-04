66067306a8e500372300ff5ca275efa2ae763ec7
Anthony Le Goas <anthony.legoas@gmail.com>
2020-10-04 19:40:51 +0200

    ✨ (common) add <ZoomOut>

---
+++ src/common/MMGame/MMMenu/ZoomOut/ZoomOut.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { IconButton } from "../../../../cdk";
+  import MMMenuEntry from "../MMMenuEntry";
+
+  export let disabled = false;
+</script>
+
+<MMMenuEntry {disabled}>
+  <IconButton ariaLabel="Dézoomer" name="minus" on:click />
+</MMMenuEntry>
---
+++ src/common/MMGame/MMMenu/ZoomOut/ZoomOutContainer.svelte
@@ -0,0 +1,11 @@
+<script>
+  import ZoomOut from "./ZoomOut.svelte";
+  import { gameStore } from "../../../../stores";
+
+  const { zoom, zoomOut, zoomMin } = gameStore;
+  let disabled = true;
+
+  zoom.subscribe(value => (disabled = value <= zoomMin));
+</script>
+
+<ZoomOut {disabled} on:click={() => zoomOut()} />
