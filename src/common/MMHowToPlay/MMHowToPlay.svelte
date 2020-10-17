3cec12b46b09d5c3d7fe48fc06291746d17b01f2
Anthony Le Goas <anthony.legoas@gmail.com>
2020-10-17 15:41:21 +0200

    ♻️ (common) refacto how-to-play page

--- src/common/MMHowToPlay/MMHowToPlay.svelte
+++ src/common/MMHowToPlay/MMHowToPlay.svelte
@@ -10,30 +10,7 @@
     align-items: center;
   }
 
-  h1 {
-    font-size: 3.5rem;
-    color: var(--primary-color);
-    margin: 0.5em 0;
-  }
-
-  h2 {
-    color: var(--primary-color);
-    font-size: 2.25rem;
-    padding-bottom: 0.25em;
-  }
-
-  h2::after {
-    background-color: var(--tertiary-color);
-    content: "";
-    display: block;
-    height: 4px;
-    margin: 1rem auto 0;
-    width: 5rem;
-  }
-
   p {
-    font-size: 1.2rem;
-    margin: 0.5em 2em;
     text-align: center;
   }
 
@@ -55,11 +32,11 @@
 </style>
 
 <div class="mm-how-to-play">
-  <h1>Comment jouer ?</h1>
+  <h2>Comment jouer ?</h2>
   <ul>
     {#each steps as step}
       <li>
-        <h2>{step.title}</h2>
+        <h3>{step.title}</h3>
         <p>{step.text}</p>
       </li>
     {/each}
