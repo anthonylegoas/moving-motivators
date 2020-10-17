adafc50ba2b902da6dd485c64ff198b8d6d269ed
Anthony Le Goas <anthony.legoas@gmail.com>
2020-10-17 16:55:15 +0200

    ✨ (components) add <GitHubLink>

---
+++ src/components/GitHubLink/GitHubLink.svelte
@@ -0,0 +1,9 @@
+<script>
+  import { IconLink } from "../../cdk";
+</script>
+
+<IconLink
+  href="https://github.com/anthonylegoas/moving-motivators"
+  name="github">
+  Voir sur GitHub
+</IconLink>
