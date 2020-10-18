a6ac972a8f128dd59eea81bd739062e9d635a769
Anthony Le Goas <anthony.legoas@gmail.com>
2020-10-18 19:31:51 +0200

    ✨ (components) add <LanguageSelector>

---
+++ src/components/LanguageSelector/LanguageSelector.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { _, locale, locales } from "svelte-i18n";
+  let selected = $locale;
+</script>
+
+<style>
+  select {
+    margin-left: 1rem;
+    padding: 0.25rem 0.5rem;
+    border-color: var(--primary-color);
+    border-radius: 4px;
+    color: var(--primary-color);
+  }
+</style>
+
+<select bind:value={selected} on:change={() => ($locale = selected)}>
+  {#each $locales as item}
+    <option value={item}>{$_(`languages.${item}`)}</option>
+  {/each}
+</select>
