88b4eaabd2db042fd6ca459550c3bfdf35ad1b39
FaustineG <faustine.godbillot@gmail.com>
2021-10-28 15:55:13 +0200

    ✨ (components) add png export of cards

---
+++ src/components/MMGame/MMMenu/ExportPNG/ExportPNG.svelte
@@ -0,0 +1,8 @@
+<script>
+  import { IconButton } from '../../../../cdk'
+  import MMMenuEntry from '../MMMenuEntry'
+</script>
+
+<MMMenuEntry>
+  <IconButton ariaLabel="Download page in PDF" name="upload" on:click />
+</MMMenuEntry>
