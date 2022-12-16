09fe9b8ec98faadd910950b33632155d6e242132
Anthony Le Goas <anthony.legoas@gmail.com>
2022-12-16 18:01:26 +0100

    🚨 fix lint errors & warnings

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,15 +1,15 @@
 describe("Navigation", () => {
-  beforeEach(() => {
-    cy.visit("/");
-  });
+    beforeEach(() => {
+        cy.visit("/");
+    });
 
-  it("navigates to /", () => {
-    cy.get("nav a").contains("THE GAME").click();
-    cy.url().should("include", "/");
-  });
+    it("navigates to /", () => {
+        cy.get("nav a").contains("THE GAME").click();
+        cy.url().should("include", "/");
+    });
 
-  it("navigates to /about", () => {
-    cy.get("nav a").contains("ABOUT").click();
-    cy.url().should("include", "/about");
-  });
+    it("navigates to /about", () => {
+        cy.get("nav a").contains("ABOUT").click();
+        cy.url().should("include", "/about");
+    });
 });
